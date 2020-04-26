c528168baf1ac474cfc31570beec97542d372a92
啸震 程 <1075527224@qq.com>
2020-04-26 18:01:20 +0800

    添加项目文件。

---
+++ Hello World/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //第一次使用Github，所以还是按着传统来，输出Hello World
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
